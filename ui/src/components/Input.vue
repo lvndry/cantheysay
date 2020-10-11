fc2e3e9a36da9aaab1210b1397882e48e13816ba
lvndry <monga.4landry@gmail.com>
2020-10-11 09:23:09 +0200

    input component

---
+++ ui/src/components/Input.vue
@@ -0,0 +1,16 @@
+<template>
+  <form class="container" novalidate>
+    Can <input name="name" class="ct-input" /> say the N word
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Input extends Vue {}
+
+</script>
+
+<style>
+</style>
--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/question_mark.png">
+    <Input />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Input from '@/components/Input.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Input,
   },
 };
 </script>
